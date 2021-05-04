630f0c60f3ee32d801963d7d679278ee57088dc5
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2021-05-04 21:08:16 +0900

    Connect DB

---
+++ backend/src/common/entities/core.entity.ts
@@ -0,0 +1,18 @@
+import { Field, Int, ObjectType } from "@nestjs/graphql";
+import { CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+
+@ObjectType()
+export class CoreEntity {
+    @Field(type => Number)
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Field(type => Date)
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @Field(type => Date)
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
--- backend/src/user/entities/user.entity.ts
+++ backend/src/user/entities/user.entity.ts
@@ -1,4 +1,7 @@
 import { Field, InputType, ObjectType, registerEnumType } from "@nestjs/graphql";
+import { IsBoolean, IsEmail, IsEnum, IsNumber, IsString } from "class-validator";
+import { CoreEntity } from "src/common/entities/core.entity";
+import { Column, Entity } from "typeorm";
 
 export enum UserRole {
     Owner = "Owner",
@@ -10,20 +13,36 @@ registerEnumType(UserRole, { name: 'UserRole' });
 
 @InputType('UserInputType', { isAbstract: true })
 @ObjectType()
-export class User {
+@Entity()
+export class User extends CoreEntity {
     @Field(type => String)
+    @Column()
+    @IsEmail()
     email: string;
 
     @Field(type => String)
+    @Column()
+    @IsString()
     password: string;
 
     @Field(type => String)
+    @Column()
+    @IsString()
     address: string;
 
     @Field(type => String)
+    @Column()
+    @IsString()
     phone: string;
 
     @Field(type => UserRole)
+    @Column()
+    @IsEnum(UserRole)
     role: UserRole;
+
+    @Field(type => Boolean)
+    @Column()
+    @IsBoolean()
+    verified: boolean;
 
 }
