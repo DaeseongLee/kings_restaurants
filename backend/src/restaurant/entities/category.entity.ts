f418e83f5f6189535b7d2926e61236090dc4f9ac
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2021-05-06 21:03:48 +0900

    Add CategoryEntity

---
+++ backend/src/restaurant/entities/category.entity.ts
@@ -0,0 +1,32 @@
+import { CoreEntity } from './../../common/entities/core.entity';
+import { Field, InputType, ObjectType } from "@nestjs/graphql";
+import { Column, Entity, OneToMany } from "typeorm";
+import { IsString } from 'class-validator';
+import { Restaurant } from './restaurant.entity';
+
+@InputType('CategoryInputType', { isAbstract: true })
+@ObjectType()
+@Entity()
+export class Category extends CoreEntity {
+    @Field(type => String)
+    @Column()
+    @IsString()
+    name: string;
+
+    @Field(type => String)
+    @Column()
+    @IsString()
+    slug: string;
+
+    @Field(type => String)
+    @Column()
+    @IsString()
+    coverImg: string;
+
+    @Field(type => [Restaurant])
+    @OneToMany(
+        type => Restaurant,
+        restaurant => restaurant.category
+    )
+    restaurants: Restaurant[]
+}
