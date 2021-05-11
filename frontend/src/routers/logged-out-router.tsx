ef3e81119d9ae5f91373ea4603fce026a260dc72
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2021-05-11 21:08:55 +0900

    Frontend Setup

---
+++ frontend/src/routers/logged-out-router.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { isLoggedInVar } from '../apollo';
+
+export const LoggedOutRouter = () => {
+    const onClick = () => {
+        isLoggedInVar(true);
+    }
+
+    return (
+        <div>
+            <h1>LoggedOut</h1>
+            <button onClick={onClick}>LogIn</button>
+        </div>
+    )
+}
